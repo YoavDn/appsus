3b0f53a90eaf07c36df64278afb846b304c061a4
AfikZehavi <afikzehavi2022@gmail.com>
2022-06-29 14:42:01 +0300

    baby steps

--- js/apps/keep/cmps/note-text.cmp.js
+++ js/apps/keep/cmps/note-text.cmp.js
@@ -1,13 +1,42 @@
+import { notesService } from "../keep-services/note.service.js"
 export default {
     template: `
-    <input type="text" placeholder="Whats on your mind...">
+        <div class="txt-container shadow">
+        <input v-if="isExpand" type="text" placeholder="title" class="title-input" @keyup.enter="onAddNote" v-model="note.info.title">
+        <input type="text" placeholder="Whats on your mind..."
+         class="txt-input" @click="isExpand = true" @keyup.enter="onAddNote" v-model="note.info.txt">
+        </div>
     `,
     data() {
         return {
-            txt: ''
+            isExpand: false, 
+            note: {
+                type: 'note-text',
+                isPinned: false,
+                info: {
+                    title: '',
+                    txt: ''
+                },
+                style: {
+                    backgroundColor: '#fff'
+                }
+            }
         }
     },
     methods: {
+        onAddNote() {
+            // console.log('txt = ', this.note.txt)
+            // console.log('this.title = ', this.note.title)
+            // console.log('note = ', this.note)
+            if (this.note.info.title === '' && this.note.info.txt === '') return
+            notesService.addNote(this.note)
+
+            this.isExpand = false
+        },
+        expand() {
+
+        }
+
     },
     computed: {
     },
