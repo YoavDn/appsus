661a9e862b3a6a3a02d428e8b65a2e0d10f5d490
yoav <yoavdan.m@gmail.com>
2022-06-29 22:54:29 +0300

    added routing

--- js/apps/mail/cmps/mail-preview.cmp.js
+++ js/apps/mail/cmps/mail-preview.cmp.js
@@ -2,21 +2,21 @@
 export default {
     props: ['mail'],
     template: `
-    <div class="item-header flex align-center">
-        <div class="item-main-actions flex">
+        <div class="item-header flex align-center">
+            <div class="item-main-actions flex">
             <button>☐</button>
-            <button>&bigstar;</button>
+            <button :class="starStyle">{{star}}</button>
         </div>
         <h2 class="maill-item-from">{{mail.from}}</h2>
-    </div>
+        </div>
 
-    <div class="item-content flex align-center">
-        <h2 class="item-subject bold">{{mail.subject}}</h2>
-        <h2 class="thin"> - {{mail.body}}</h2>
-    </div>
-    <div class="item-date">
-        <h2>{{stringDate}}</h2>
-    </div>
+        <div class="item-content flex align-center">
+            <h2 class="item-subject bold">{{mail.subject}}</h2>
+            <h2 class="thin"> - {{mail.body}}</h2>
+        </div>
+        <div class="item-date">
+            <h2>{{stringDate}}</h2>
+        </div>
     `,
     data() {
         return {
@@ -31,6 +31,14 @@ export default {
             const strMonth = date.toLocaleString('default', { month: 'short' });
             return day + " " + strMonth
         },
+        star() {
+            return this.mail.isStar ? '&bigstar;' : '&star;'
+        },
+        starStyle() {
+            return { star: this.mail.isStar, notStar: !this.mail.isStar }
+        }
+
+
     },
     created() {
     },
--- js/router.js
+++ js/router.js
@@ -2,19 +2,33 @@
 import homePage from "./pages/app-home.cmp.js"
 import mailPage from "./apps/mail/pages/mail-index.cmp.js"
 import keepPage from "./apps/keep/pages/note-index.cmp.js"
+import mailDetails from "./apps/mail/pages/mail-details.cmp.js"
+import mailList from "./apps/mail/cmps/mail-list.cmp.js"
 
 
 const routes = [
     {
-        path:"/",
+        path: "/",
         component: homePage,
-    },  
-    {
-        path:"/mail",
-        component: mailPage,
     },
-     {
-        path:"/keep",
+    {
+        path: "/mail",
+        component: mailPage,
+        children: [
+            {
+                path: 'mails',
+                component: mailList,
+                props: true
+            },
+            {
+                path: ':mailId',
+                component: mailDetails,
+            },
+        ]
+    },
+
+    {
+        path: "/keep",
         component: keepPage,
     },
 ]
