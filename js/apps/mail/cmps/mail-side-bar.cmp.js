e3925c1773c3b353ee233fd5de28412e0637e851
yoav <yoavdan.m@gmail.com>
2022-06-29 18:43:08 +0300

    added sidebar btns

--- js/apps/mail/cmps/mail-side-bar.cmp.js
+++ js/apps/mail/cmps/mail-side-bar.cmp.js
@@ -4,7 +4,10 @@ export default {
     <section class="mail-side-bar">
         <button class="compose-btn shadow" @click="$emit('openNewMail')"> <span>&plus;</span> Compose</button>
         <div class="side-bar-items">
-            <h2>will add links</h2>
+            <button class="side-bar-btn bold"><span>D</span>Inbox</button>
+            <button class="side-bar-btn"><span>&bigstar;</span>Starred</button>
+            <button class="side-bar-btn"><span>D</span>Sent</button>
+            <button class="side-bar-btn"><span>D</span>Drafts</button>
         </div>
     </section>
     `,
