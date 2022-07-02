7ece29ad60ebe8bee3e0d57a6cf3a95390db2e3f
yoav <yoavdan.m@gmail.com>
2022-07-02 20:20:42 +0300

    star postion on mobile

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -3,18 +3,15 @@ export default {
     template: `
     <section class="header-container">
         <header class="main-header flex space-between">
-            
         <div  @click="goToHomePage" class="logo-box flex">
             <img src="assest/images/circles.svg" alt="">
-    <h2 class="logo">Appsus </h2>
-
+            <h2 class="logo">Appsus </h2>
         </div>
             <nav class="main-nav">
                 <ul class="nav-list flex" >
                     <router-link class="router-link mail-nav-link" to="/mail/mails">Mail <span v-if="unRead" class="header-notifictions">{{unRead}}</span></router-link>
                     <router-link class="router-link" to="/keep">Keep</router-link>
                     <router-link class="router-link" to="/book">Books</router-link>
-
                 </ul>
             </nav>
         </header> 
@@ -36,5 +33,4 @@ export default {
             this.unRead = count
         })
     },
-
 }
